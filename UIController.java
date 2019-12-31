f1ad6b1542db559d0e0f9f9317f07fce8c300ae2
teamhimeh <akimuhirai@a-hirai-MacBookPro.local>
2019-12-31 23:05:39 +0900

    ListViewで選択中の駅を赤点で表示する

--- UIController.java
+++ UIController.java
@@ -787,6 +787,10 @@ public class UIController implements Initializable{
 				if(!obeyLine) {
 					staTextLocation.selectToggle(stlToggles[s.getTextLocation()-Station.TEXT_LEFT]);
 				}
+				//選択中の駅を赤点で表示する
+				movingStList.clear();
+				movingStList.add(new MvSta(s));
+				lineDraw();
 			});
 		double[] startCoor = new double[2];//ドラッグスタート時の座標を記録する。station座標（zoomを考慮）．
 		draggedRect.setVisible(false);
